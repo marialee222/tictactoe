ad7a0309857df8f001a44be46511c1bd8fa56653
Maria Lee <juggleisreal@gmail.com>
2024-04-09 20:58:07 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css'; // Import your CSS file
-import Game from './tictactoe'; // Import your main Tic-tac-toe component
+import { Game } from './tictactoe';
 
 ReactDOM.render(
   <React.StrictMode>
